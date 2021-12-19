5ed2b0bf4bdf1987c913eb189a8c5bf0f35df108
efatuarte <fatuarte.feern21@triana.salesianos.edu>
2021-12-19 22:46:03 +0100

    Solucionado validador de coordenadas

--- triana-tourist/src/main/java/com/salesianostriana/dam/trianatourist/validaciones/simple/validadores/IsCoordinateValidator.java
+++ triana-tourist/src/main/java/com/salesianostriana/dam/trianatourist/validaciones/simple/validadores/IsCoordinateValidator.java
@@ -4,11 +4,10 @@ import com.salesianostriana.dam.trianatourist.validaciones.simple.anotaciones.Is
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class IsCoordinateValidator implements ConstraintValidator<IsCoordinate, String> {
-
-    private String LATITUDE_PATTERN="^(\\+|-)?(?:90(?:(?:\\.0{1,6})?)|(?:[0-9]|[1-8][0-9])(?:(?:\\.[0-9]{1,6})?))$";
-    private String LONGITUDE_PATTERN="^(\\+|-)?(?:180(?:(?:\\.0{1,6})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\\.[0-9]{1,6})?))$";
 
     @Override
     public void initialize(IsCoordinate constraintAnnotation) { }
@@ -16,9 +15,26 @@ public class IsCoordinateValidator implements ConstraintValidator<IsCoordinate, 
     @Override
     public boolean isValid(String location, ConstraintValidatorContext context) {
 
-        String latitude = location.substring(0, location.indexOf(","));
-        String longitude = location.substring(0, location.indexOf(",") + 1);
+//        String LATITUDE_PATTERN="^(\\+|-)?(?:90(?:(?:\\.0{1,6})?)|(?:[0-9]|[1-8][0-9])(?:(?:\\.[0-9]{1,6})?))$";
+//        String LONGITUDE_PATTERN="^(\\+|-)?(?:180(?:(?:\\.0{1,6})?)|(?:[0-9]|[1-9][0-9]|1[0-7][0-9])(?:(?:\\.[0-9]{1,6})?))$";
+//
+//        Pattern patronLatitud = Pattern.compile(LATITUDE_PATTERN);
+//        Pattern patronLongitud = Pattern.compile(LONGITUDE_PATTERN);
+//
+//        String latitude = location.substring(0, location.indexOf(","));
+//        String longitude = location.substring(0, location.indexOf(",") + 1);
+//
+//        Matcher matcherLatitud = patronLatitud.matcher(latitude);
+//        Matcher matcherLongitud = patronLongitud.matcher(longitude);
+//
+//        return matcherLatitud.find() && matcherLongitud.find();
 
-        return latitude.matches(LATITUDE_PATTERN) && longitude.matches(LONGITUDE_PATTERN);
+        String s = "^([-+]?\\d{1,2}[.]\\d+),\\s*([-+]?\\d{1,3}[.]\\d+)$";
+
+        Pattern p = Pattern.compile(s);
+
+        Matcher m = p.matcher(location);
+
+        return m.find();
     }
 }
