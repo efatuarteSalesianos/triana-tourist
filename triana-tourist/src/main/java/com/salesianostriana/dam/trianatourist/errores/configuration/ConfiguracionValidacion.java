aae2ed7ab39469ae2fba6121be6819e3ee713979
efatuarte <fatuarte.feern21@triana.salesianos.edu>
2021-12-20 14:24:37 +0100

    Comenzada internacionalización

--- triana-tourist/src/main/java/com/salesianostriana/dam/trianatourist/errores/configuration/ConfiguracionValidacion.java
+++ triana-tourist/src/main/java/com/salesianostriana/dam/trianatourist/errores/configuration/ConfiguracionValidacion.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
 public class ConfiguracionValidacion {
@@ -22,5 +23,12 @@ public class ConfiguracionValidacion {
         LocalValidatorFactoryBean validator = new LocalValidatorFactoryBean();
         validator.setValidationMessageSource(messageSource());
         return validator;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
     }
 }
